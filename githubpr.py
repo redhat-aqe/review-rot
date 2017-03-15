98eda6a8781e307a71cbe1612fdee5e403c9e5ff
nirzari18 <niyer@redhat.com>
2017-03-15 18:36:39 -0400

    Removed requirement for github auth for public repos

--- githubpr.py
+++ githubpr.py
@@ -1,19 +1,25 @@
+import config
 from github import Github
 
-g = Github("username", "password")
+#The line below is commented out for future scope of private repo
+#g = Github(config.github['uname'], config.github['pwd'])
+
+g = Github()
 
 repolist = []
-repolist.append("repository_name")
-for repo in repolist:
+repolist.append("Random-Queries-Using-Amazon-Web-Service")
+repolist.append("Google-Charts")
 
+for repo_name in repolist:
     try:
         #get Repository object for given user/organization and repo name
-        repo = g.get_user().get_repo(repo)
+        #repo = g.get_user().get_repo(repo_name)
+        repo = g.get_user("nirzari18").get_repo(repo_name)
     
         #Get list of open pull requests for a given repo
-        pullRequests = repo.get_pulls()
+        pull_requests = repo.get_pulls()
         
-        for pr in pullRequests:
+        for pr in pull_requests:
             user = pr.user.login
             title = pr.title
             url = pr.html_url
@@ -28,4 +34,4 @@ for repo in repolist:
             print "@" + user + " filed '" + title + "' " + url + " on " + days + comments
 
     except Exception as e:
-        print e
+        print "Something is wrong with the repository: " + repo_name
