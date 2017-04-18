66ae82fdd3e62859a6a016e63a62202b61966c56
nirzari <niyer@redhat.com>
2017-04-18 13:58:45 -0400

    Added basic structure and script for reviewrot

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(name='review-rot',
+      version='0.0',
+      author='Nirzari Iyer',
+      author_email='niyer@redhat.com',
+      description=('CLI tool to list review(pull) requests from '
+                   'github,gitlab,pagure and gerrit'),
+      long_description=long_description,
+      license='GNU GENERAL PUBLIC LICENSE Version 3',
+      url='https://github.com/nirzari/review-rot',
+      packages=find_packages(),
+      install_requires=[],
+      scripts=['bin/review-rot']
+      )
