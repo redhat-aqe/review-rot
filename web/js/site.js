02f27ba5e4411340f3bf4a434709240980f8768c
Ralph Bean <rbean@redhat.com>
2017-06-29 21:17:05 -0400

    A basic site.

---
+++ web/js/site.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	var entry_template = Handlebars.compile($("#entry-template").html());
+	var footer_template = Handlebars.compile($("#footer-template").html());
+
+	var xhr = $.ajax({
+		dataType: 'json',
+		// TODO -- this is temporary while WiP.
+		url: 'http://threebean.org/example-reviewrot.json',
+		error: function() {
+			$('error-message').removeClass('hidden');
+		},
+		success: function(data) {
+			var modified = xhr.getResponseHeader("Last-Modified")
+			$('.footer').append(footer_template({
+				generated: moment(modified).fromNow()
+			}));
+			$.each(data, function(key, value) {
+				$('#reviews').append(entry_template(value));
+			});
+		}
+	})
+});
+
