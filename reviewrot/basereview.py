redhat-aqe/review-rot

commit eeb515ccd0ca539c71f96394b1334b3705fdcf9b

Error Handling with reviewers suggested changes

reviewrot/basereview.py
@@ -1,12 +1,16 @@
-from collections import OrderedDict
 import datetime
-from dateutil.relativedelta import relativedelta
-import time
 import json
-from requests.exceptions import HTTPError
+import logging
+import time
+
+from collections import OrderedDict
+
+from dateutil.relativedelta import relativedelta
+
+log = logging.getLogger(__name__)
+
 
 class BaseService(object):
-
     def check_request_state(self, created_at,
                             state_, value, duration):
         """
@@ -25,8 +29,7 @@ class BaseService(object):
             True if the review request is older or newer than
             specified time interval, False otherwise
         """
-        if (state_ is not None and value is not None and
-                duration is not None):
+        if (state_ is not None and value is not None and duration is not None):
             """
             find the relative time difference between now and
             review request filed to retrieve relative information
@@ -51,7 +54,7 @@ class BaseService(object):
             elif duration == 'm':
                 """ use rel_diff to calculate absolute time difference
                 in months """
-                abs_month = (rel_diff.years*12) + rel_diff.months
+                abs_month = (rel_diff.years * 12) + rel_diff.months
                 if state_ == 'older' and abs_month < value:
                     return False
                 elif state_ == 'newer' and abs_month >= value:
@@ -66,7 +69,7 @@ class BaseService(object):
             elif duration == 'h':
                 """ use abs_diff to calculate absolute time difference
                 in hours """
-                abs_hour = abs_diff.total_seconds()/3600
+                abs_hour = abs_diff.total_seconds() / 3600
                 if state_ == 'older' and abs_hour < value:
                     return False
                 elif state_ == 'newer' and abs_hour >= value:
@@ -74,7 +77,7 @@ class BaseService(object):
             elif duration == 'min':
                 """ use abs_diff to calculate absolute time difference
                 in minutes """
-                abs_min = abs_diff.total_seconds()/60
+                abs_min = abs_diff.total_seconds() / 60
                 if state_ == 'older' and abs_min < value:
                     return False
                 elif state_ == 'newer' and abs_min >= value:
@@ -83,33 +86,30 @@ class BaseService(object):
                 raise ValueError("Invalid duration type: %s" % duration)
         return True
 
-    def _decode_response(self,response):
-
-        GERRIT_JSON_PREFIX = ")]}\'\n"
-
-        """ Remove Gerrit's prefix and convert to JSON.
+    def _decode_response(self, response):
+        """
+        Remove Gerrit's prefix and convert to JSON.
         :returns:
             Converted JSON content
         :raises:
-            requests.HTTPError if the response contains an HTTP error status code.
+            ValueError if the content is not in proper json format.
         """
+        gerrit_json_prefix = ")]}\'\n"
         content = response.content.strip()
-        if response.encoding:
-            content = content.decode(response.encoding)
-        response.raise_for_status()
-        content_type = response.headers.get('content-type', '')
-        if content_type.split(';')[0] != 'application/json':
-            return content
-        if content.startswith(GERRIT_JSON_PREFIX):
-            content = content[len(GERRIT_JSON_PREFIX):]
         try:
+            if response.encoding:
+                content = content.decode(response.encoding)
+            response.raise_for_status()
+            content_type = response.headers.get('content-type', '')
+            if content_type.split(';')[0] != 'application/json':
+                return content
+            if content.startswith(gerrit_json_prefix):
+                content = content[len(gerrit_json_prefix):]
             return json.loads(content)
         except ValueError:
-            logging.error('Invalid json content: %s' % content)
-            raise
+            raise ValueError('Invalid json content: %s' % content)
 
-
-    def _call_api(self, url, method='GET', ssl_verify=True):
+    def _call_api(self, url, method='GET', ssl_verify=True, ignore_error=False):
         """
         Method used to call the API.
         It returns the raw JSON returned by the API or raises an exception
@@ -120,24 +120,29 @@ class BaseService(object):
         Returns:
             raw JSON returned by API
         """
-        response = self.session.request(method=method,url=url,headers=self.header,verify=ssl_verify)
-        decoded_response = None
+        decoded_response = ''
         try:
-
+            response = self.get_response(method, url, ssl_verify)
             decoded_response = response.json()
-        except Exception as err:
-            try:
-                decoded_response = self._decode_response(response)
-            except HTTPError as http_error:
-                return ''
-            except Exception as e:
-                log.exception('Error while decoding JSON: {0}'.format(err))
+        except ValueError:
+            if response.status_code == 404:
+                # usually happens when comments are not found for Gerrit Change Request
+                if not ignore_error:
+                    raise ValueError('Page not found: %s' % response.url)
+            elif response.status_code == 200:
+                try:
+                    decoded_response = self._decode_response(response)
+                except Exception as e:
+                    raise ValueError('Error while decoding JSON: {0}'.format(e))
+            else:
                 raise
-        if response.status_code != 200:
-            if 'error_code' in decoded_response:
-                log.debug(decoded_response['error'])
-                raise Exception(decoded_response['error'])
+        except:
+            raise
         return decoded_response
+
+    def get_response(self, method, url, ssl_verify):
+        return self.session.request(method=method, url=url,
+                                    headers=self.header, verify=ssl_verify)
 
 
 class BaseReview(object):
@@ -200,7 +205,7 @@ class BaseReview(object):
 
     def _format_oneline(self):
         string = "@%s filed '%s' %s since %s" % (
-                self.user, self.title, self.url, self.since)
+            self.user, self.title, self.url, self.since)
 
         if self.comments == 1:
             string += " with %s comment" % self.comments
@@ -211,7 +216,7 @@ class BaseReview(object):
 
     def _format_indented(self):
         string = "@%s filed '%s'\n\t%s\n\tsince %s" % (
-                self.user, self.title, self.url, self.since)
+            self.user, self.title, self.url, self.since)
 
         if self.comments == 1:
             string += "\n\twith %s comment" % self.comments

test/test.py
@@ -148,6 +148,7 @@ class PagureTest(TestCase):
                                    repo_name=self.config['repo_name'])
         self.assertTrue('Project not found' in str(context.exception))
 
+
 class GerritTest(TestCase):
     def setUp(self):
         filename = join(dirname(__file__), 'test_gerrittest.yaml')
@@ -156,6 +157,25 @@ class GerritTest(TestCase):
 
     def test_gerrit_object_create(self):
         self.assertTrue(isinstance((get_git_service('gerrit')), GerritService))
+
+    def test_gerrit_incorrect_host_url(self):
+        gerrit = GerritService()
+        with self.assertRaises(Exception)as context:
+            self.assertRaises(ValueError, gerrit.request_reviews(repo_name=self.config['repo_name'],
+                                                                 host=self.config['incorrect_host']))
+        self.assertTrue( self.config['incorrect_host_msg']  in str(context.exception))
+
+    def test_gerrit_incorrect_repo_name(self):
+        gerrit = GerritService()
+        with self.assertRaises(Exception)as context:
+            self.assertRaises(ValueError, gerrit.request_reviews(repo_name=self.config['incorrect_repo_name'],
+                                                                 host=self.config['host']))
+        self.assertTrue(self.config['incorrect_repo_name_msg'] in str(context.exception))
+
+    def test_gerrit_request_reviews(self):
+        gerrit = GerritService()
+        result = gerrit.request_reviews(repo_name=self.config['repo_name'], host=self.config['host'])
+        self.assertTrue(result is not None)
 
 if __name__ == '__main__':
     unittest.main()

bin/review-rot
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+import argparse
+import logging
 import operator
 import os
-import logging
-import argparse
-import yaml
 from os.path import expanduser, expandvars
+
 from reviewrot import get_git_service
+
+import yaml
 
 
 def main(state_, value, duration, ssl_verify=None):
@@ -64,21 +66,22 @@ def main(state_, value, duration, ssl_verify=None):
                 """
                 get pull/merge/change requests for specified git service
                 """
-                repo_name =  data if res.get('repo_name') is None else res.get('repo_name')
+                repo_name = data
+                if res.get('repo_name') is not None:
+                    repo_name = res.get('repo_name')
+
                 results.extend(
                     git_service.request_reviews(
-                        user_name=res.get('user_name',None),
+                        user_name=res.get('user_name'),
                         repo_name=repo_name,
                         state_=state_,
                         value=value,
                         duration=duration,
-                        token=item.get('token',None),
+                        token=item.get('token'),
                         host=item.get('host'),
                         ssl_verify=ssl_verify,
                     )
                 )
-
-
 
     # Now, with all results in place, sort them and print
     for result in sorted(results, key=operator.attrgetter('time')):

examples/gerrit_sample.yaml
@@ -2,5 +2,3 @@
   host: https://gerrit-review.googlesource.com
   repos:
      - gerrit
-  format: indented
-  ssl_verify: False

examples/sampleinput.yaml
@@ -17,4 +17,9 @@
   host: null
   repos:
     - repo_name
-    - namespace/repo_name                  
+    - namespace/repo_name
+
+- type: gerrit
+  host: null
+  repos:
+    - project_name

reviewrot/gerritstack.py
@@ -1,10 +1,13 @@
-from datetime import datetime
 import logging
+
+from datetime import datetime
+
 import requests
-import json
-from reviewrot.basereview import BaseService, BaseReview
+
+from reviewrot.basereview import BaseReview, BaseService
 
 log = logging.getLogger(__name__)
+
 
 class GerritService(BaseService):
     """
@@ -15,30 +18,53 @@ class GerritService(BaseService):
         self.session = requests.session()
         self.header = None
 
-    def request_reviews(self,host,repo_name, state_=None,user_name=None,token=None,
-                        value=None, duration=None,ssl_verify=True):
-        self.url = host;
+    def request_reviews(self, host, repo_name, state_=None,
+                        user_name=None, token=None, value=None,
+                        duration=None, ssl_verify=True):
+        self.url = host
+        reviews = None
 
-        if repo_name is not None:
-            request_url = "{}/changes/?q=project:{}+status:open&o=DETAILED_ACCOUNTS".format(self.url,repo_name)
-            log.debug('Looking for change requests for %s -> %s', self.url, repo_name) 
+        if self.check_host_url(ssl_verify) and self.check_repo_exist(repo_name, ssl_verify):
+            request_url = "{}/changes/?q=project:{}+status:open&o=DETAILED_ACCOUNTS".format(self.url, repo_name)
+            log.debug('Looking for change requests for %s -> %s', self.url, repo_name)
             review_response = self._call_api(url=request_url, ssl_verify=ssl_verify)
-            return self.format_response(review_response,state_,value,duration)
+            reviews = self.format_response(review_response, state_, value, duration)
+        return reviews
 
-    def get_comments_count(self,change_id):
-        request_url = "{}/changes/{}/comments".format(self.url,str(change_id))
-        decoded_response = self._call_api(request_url)
+    def check_repo_exist(self, repo_name, ssl_verify):
+        request_url = "{}/projects/{}".format(self.url, repo_name)
+        log.debug('Checking if repo %s exists', repo_name)
+        try:
+            self._call_api(url=request_url, ssl_verify=ssl_verify)
+            return True
+        except ValueError:
+            raise ValueError("No repo found. Please check the repo name in ~/.reviewrot.yaml file.")
+
+    def check_host_url(self, ssl_verify):
+        log.debug('Checking if host URL %s is correct', self.url)
+        try:
+            response = self.get_response(method='GET', url=self.url, ssl_verify=ssl_verify)
+            if response.status_code == 200:
+                return True
+            else:
+                raise ValueError('Host URL is incorrectly configured in ~/.reviewrot.yaml file.')
+        except:
+            raise ValueError('Host URL is incorrectly configured in ~/.reviewrot.yaml file.')
+
+    def get_comments_count(self, change_id):
+        request_url = "{}/changes/{}/comments".format(self.url, str(change_id))
+        decoded_response = self._call_api(request_url, ignore_error=True)
         total_comments = 0
         for response in decoded_response:
             if response is not '/COMMIT_MSG':
-                total_comments = total_comments + len(decoded_response.get(response))            
-   
+                total_comments = total_comments + len(decoded_response.get(response))
+
         return total_comments
-                 
-    def format_response(self, decoded_responses,state_,value,duration):
+
+    def format_response(self, decoded_responses, state_, value, duration):
         res_ = []
         for decoded_response in decoded_responses:
-            created_date = datetime.strptime(decoded_response['created'][:-3] , "%Y-%m-%d %H:%M:%S.%f")
+            created_date = datetime.strptime(decoded_response['created'][:-3], "%Y-%m-%d %H:%M:%S.%f")
             result = self.check_request_state(created_date, state_, value, duration)
             if result is False:
                 log.debug("Change request '%s' is not %s than specified time interval", decoded_response['subject'], state_)
@@ -46,11 +72,13 @@ class GerritService(BaseService):
             owner = decoded_response['owner']
             res = GerritReview(user=owner.get('username', owner.get('email')),
                                title=decoded_response['subject'],
-                               url=self.url + '/' + str(decoded_response['_number']),
-                               time= created_date,
+                               url="{}/{}".format(self.url,
+                                                  str(decoded_response['_number'])),
+                               time=created_date,
                                comments=self.get_comments_count(decoded_response['id']))
             res_.append(res)
         return res_
+
 
 class GerritReview(BaseReview):
     pass

reviewrot/pagurestack.py
@@ -1,8 +1,10 @@
-import os
-import logging
 import datetime
+import logging
+import os
+
 import requests
-from reviewrot.basereview import BaseService, BaseReview
+
+from reviewrot.basereview import BaseReview, BaseService
 
 log = logging.getLogger(__name__)
 
@@ -58,7 +60,7 @@ class PagureService(BaseService):
             request_url = "{}/api/0/{}/pull-requests".format(self.instance,
                                                              repo_name)
             log.debug('Looking for pull requests for %s -> %s',
-                      'pagure.io',  repo_name)
+                      'pagure.io', repo_name)
         log.debug('Calling API with request_url: %s', request_url)
         response = self._call_api(url=request_url, ssl_verify=ssl_verify)
         res_ = []
@@ -98,6 +100,7 @@ class PagureService(BaseService):
             log.debug(res)
             res_.append(res)
         return res_
+
 
 class PagureReview(BaseReview):
     pass

test/test_gerrittest.yaml
@@ -1,2 +1,8 @@
 host: https://gerrit-review.googlesource.com
 repo_name: gerrit
+
+incorrect_repo_name: gerritt
+incorrect_repo_name_msg: No repo found. Please check the repo name in ~/.reviewrot.yaml file.
+
+incorrect_host: https://gerrit-review.g00glesource.com
+incorrect_host_msg: Host URL is incorrect

commit cc2f8376a28f675752fe9f25cb32ea3d33100462

Get the test suite working again.
You can run it with ``detox``.

I think this may have gotten broken in eeb515ccd0ca.

reviewrot/basereview.py
@@ -123,6 +123,11 @@ class BaseService(object):
         try:
             response = self.get_response(method, url, ssl_verify)
             decoded_response = response.json()
+            # Some services (like pagure) return valid JSON with a 404 error.
+            # https://pagure.io/api/0/username/reponame/pull-requests
+            if not bool(response):
+                raise ValueError("%r gave %r: %r" % (
+                    response.request, response, decoded_response))
         except ValueError:
             if response.status_code == 404:
                 # happens when comments are not found for Gerrit Change Request

test/test.py
@@ -146,7 +146,7 @@ class PagureTest(TestCase):
         with self.assertRaises(Exception) as context:
             pagure.request_reviews(user_name=self.config['user_name'],
                                    repo_name=self.config['repo_name'])
-        self.assertTrue('Project not found' in str(context.exception))
+        self.assertIn('Page not found', str(context.exception))
 
 
 class GerritTest(TestCase):